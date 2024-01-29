f56fde3f5c44d0e24137f3fc0b19701db235e8e6
Zohar Williams <zoharkiks@gmail.com>
2024-01-29 11:19:08 +0530

    Add NAvbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between p-4 border-b bg-background '>
+<span className='text-xl font-bold'>Alemeno</span>
+<Button>Dashboard</Button>
     </div>
   )
 }
